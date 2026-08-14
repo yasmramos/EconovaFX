31ff45dff4033932ae451ccee4ab1442f8ec3455
yasmramos <yasmramos95@gmail.com>
2026-08-14 11:31:40 +0000

    fix: add missing HBox import in AccountingClosuresController

--- src/main/java/com/econovafx/modules/accounting/controller/AccountingClosuresController.java
+++ src/main/java/com/econovafx/modules/accounting/controller/AccountingClosuresController.java
@@ -7,6 +7,7 @@ import jakarta.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
